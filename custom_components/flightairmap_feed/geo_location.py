aef9c56b287a79649eb8b3fb38932aec3b6e388c
bavotto <71755886+bavotto@users.noreply.github.com>
2020-09-26 17:06:47 +1000

    Update geo_location.py

--- custom_components/flightairmap_feed/geo_location.py
+++ custom_components/flightairmap_feed/geo_location.py
@@ -19,7 +19,8 @@ from homeassistant.const import (
     EVENT_HOMEASSISTANT_STOP,
 )
 from homeassistant.core import callback
-from homeassistant.helpers import ConfigType, aiohttp_client, config_validation as cv
+from homeassistant.helpers import aiohttp_client, config_validation as cv
+from homeassistant.helpers.typing import ConfigType, HomeAssistantType
 from homeassistant.helpers.dispatcher import (
     async_dispatcher_connect,
     async_dispatcher_send,
