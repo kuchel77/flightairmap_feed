f4903f31b15bb7137c727f1bf8648c0d4a39c478
Mark Kuchel <mark@kuchel.net>
2021-12-16 11:32:32 +1100

    Change device_ to extra_ for attributes

--- custom_components/flightairmap_feed/geo_location.py
+++ custom_components/flightairmap_feed/geo_location.py
@@ -269,7 +269,7 @@ class FlightAirMapLocationEvent(GeolocationEvent):
         return DEFAULT_UNIT_OF_MEASUREMENT
 
     @property
-    def device_state_attributes(self):
+    def extra_state_attributes(self):
         """Return the device state attributes."""
         attributes = {}
         for key, value in (
